67b6f6800fcddaff8a01792c696705f8bb9476f4
Il Drago D'oro <vicen333@gmail.com>
2021-02-17 11:32:13 +0100

    Finished the welcom view and made a carousel to the view cars

--- BlogCars/resources/views/cars.blade.php
+++ BlogCars/resources/views/cars.blade.php
@@ -1,3 +1,48 @@
 @include('header')
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>The best cars of the world!!</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div id="carousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="mercedes.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="audi.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="tesla.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 @include('footer')
--- BlogCars/resources/views/inicio.blade.php
+++ BlogCars/resources/views/inicio.blade.php
@@ -13,22 +13,26 @@
       <img src="mercedes.jpg" alt="mercedes" height="250px" width="300px">
     </div>
     <div class="col-md-4 text-center">
-      <img src="audi.jpg" alt="mercedes" height="250px" width="300px">
+      <img src="audi.jpg" alt="audi" height="250px" width="300px">
     </div>
     <div class="col-md-4 text-center">
-      <img src="tesla.jpg" alt="mercedes" height="250px" width="300px">
+      <img src="tesla.jpg" alt="tesla" height="250px" width="300px">
     </div>
   </div>
 </div>
 <div class="container">
   <div class="row mt-3">
     <div class="col-12 h2 text-center">
-      Lorem ipsum
+      This is our WebPage abour €P¡C K@R$
     </div>
   </div>
   <div class="row">
     <div class="col-12 text-center">
-      Aqui alguien podrá un Lorem o Ankush a ti se te da bien redactar tus historias xD
+      <p>The best cars in the world are made in the United States of America!!</p>
+      <p>Tesla, Lamborghini, Ferrari... We make lots and lots of cars...</p>
+      <p>And all cars are made with chinese pieces!!</p>
+      <p>Buy a plastic car for 100 times the original price!</p>
+
     </div>
   </div>
 </div>
