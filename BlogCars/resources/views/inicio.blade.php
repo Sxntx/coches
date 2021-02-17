0c258e9e25fdcd6a6d0c4236b0625da044fe614d
Il Drago D'oro <vicen333@gmail.com>
2021-02-17 12:33:09 +0100

    I put 6 new  images and make a better carousel

---
+++ BlogCars/resources/views/inicio.blade.php
@@ -0,0 +1,40 @@
+@include('header')
+<div class="container mt-3">
+  <div class="row text-center">
+    <div class="col-12 h1">
+      Best blog cars ever.
+    </div>
+  </div>
+</div>
+
+<div class="container mt-4">
+  <div class="row">
+    <div class="col-md-4 text-center">
+      <img src="mercedes.jpg" alt="mercedes" height="250px" width="300px">
+    </div>
+    <div class="col-md-4 text-center">
+      <img src="audi.jpg" alt="audi" height="250px" width="300px">
+    </div>
+    <div class="col-md-4 text-center">
+      <img src="tesla.jpg" alt="tesla" height="250px" width="300px">
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row mt-3">
+    <div class="col-12 h2 text-center">
+      This is our WebPage abour €P¡C K@R$
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 text-center">
+      <p>The best cars in the world are made in the United States of America!!</p>
+      <p>Tesla, Lamborghini, Ferrari... We make lots and lots of cars...</p>
+      <p>And all cars are made with chinese pieces!!</p>
+      <p>Buy a plastic car for 100 times the original price!</p>
+
+    </div>
+  </div>
+</div>
+
+@include('footer')
