792abf7f0f4e577576c9c22c246b85bbf645c01c
sxntx <47829537+Sxntx@users.noreply.github.com>
2021-02-17 09:57:15 +0100

    main view/controller done successfully

---
+++ BlogCars/resources/views/inicio.blade.php
@@ -0,0 +1,19 @@
+@include('header')
+<div class="container">
+  <div class="row text-center">
+    <div class="col-12 h1">
+      Best blog cars ever.
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row text-center">
+    <div class="col-12">
+      Poner imagen o carruseel de imagenes que solo ocupe en 70% del width height me da igual
+      Un buen lorem y listo
+    </div>
+  </div>
+</div>
+
+@include('footer')
